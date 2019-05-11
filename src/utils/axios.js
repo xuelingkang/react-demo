0d72b229c5c102bb51a542d88260c1ae8fb8a72d
xuelingkang <574290057@qq.com>
2019-05-11 21:00:41 +0800

    修改axios

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -21,10 +21,9 @@ let instance = axios.create({
 instance.interceptors.request.use((config) => {
     // 请求头增加token
     config.headers['token'] = cache.get(TOKEN);
-    // form表单格式提交。
-    const contentType = config.headers['Content-Type'];
-    if (contentType.indexOf(FORM_CONTENT_TYPE)!==-1 || FORM_CONTENT_TYPE.indexOf(contentType)!==-1) {
-        // 对表单格式的数据进行处理。
+    // form表单格式提交
+    if (config.headers['Content-Type']===FORM_CONTENT_TYPE) {
+        // 对表单格式的数据进行处理
         config.data = qs.stringify(config.data);
     }
     return config;
@@ -43,37 +42,37 @@ instance.interceptors.response.use(response => {
     return Promise.reject(error);
 });
 
-async function axiosGet(url, payload, config) {
+async function axiosGet(url, params, config) {
     return instance.get(url, {
-        payload,
+        params,
         ...config,
     });
 }
 
-async function axiosDelete(url, payload, config) {
+async function axiosDelete(url, params, config) {
     return instance.delete(url, {
-        payload,
+        params,
         ...config,
     });
 }
 
-async function axiosHead(url, payload, config) {
+async function axiosHead(url, params, config) {
     return instance.head(url, {
-        payload,
+        params,
         ...config,
     });
 }
 
-async function axiosPost(url, payload, config) {
-    return instance.post(url, payload, config);
+async function axiosPost(url, params, config) {
+    return instance.post(url, params, config);
 }
 
-async function axiosPut(url, payload, config) {
-    return instance.put(url, payload, config);
+async function axiosPut(url, params, config) {
+    return instance.put(url, params, config);
 }
 
-async function axiosPatch(url, payload, config) {
-    return instance.patch(url, payload, config);
+async function axiosPatch(url, params, config) {
+    return instance.patch(url, params, config);
 }
 
 export { axiosGet, axiosDelete, axiosHead, axiosPost, axiosPut, axiosPatch };
