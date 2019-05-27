22156893e5836404da5f18d08cd0352fd2559a29
xuelingkang <574290057@qq.com>
2019-05-27 15:15:29 +0800

    清除无用目录

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,54 +1,18 @@
 import {createRoutes} from '@/utils/core';
 import BasicLayout from '@/layouts/BasicLayout';
 import UserLayout from '@/layouts/UserLayout';
-import Page403 from './Pages/403';
 import NotFound from './Pages/404';
-import Page500 from './Pages/500';
-import ScreenLock from './Widgets/ScreenLock';
-import Coming from './Widgets/Coming';
-import Gallery from './Widgets/Gallery';
-import Result from './Widgets/Result';
-import LevelRoute from './Widgets/LevelRoute';
-
-// 登录
-import Login from './Sign/Login';
-// 注册
-import Register from './Sign/Register';
-// 找回密码
-import RetrievePassword from './Sign/RetrievePassword';
-
-// 部门管理
-import Dept from './Crud/Dept';
-// 用户管理
-import User from './Crud/User';
-// 角色管理
-import Role from './Crud/Role';
-// 权限（资源）管理
-import Resource from './Crud/Resource';
-// 附件管理
-import Attachment from './Crud/Attachment';
-// 邮件管理
-import Mail from './Crud/Mail';
-
 import Dashboard from './Dashboard';
-import Blank from './Blank';
-import Toolbar from './Widgets/Toolbar';
-import BaseComponent from './Widgets/BaseComponent';
-import Column from './Widgets/Column';
-import TransferTree from './Widgets/TransferTree';
-import SearchBar from './Widgets/SearchBar';
-import DataTable from './Widgets/DataTable';
-import Form from './Widgets/Form';
-import EC from './Widgets/Charts/EC';
-import G2 from './Widgets/Charts/G2';
-import Print from './Widgets/Print';
-import Banner from './Widgets/Banner';
-import Icon from './UI/Icon';
-import Mask from './UI/Mask';
-import Editor from './UI/Editor';
-import CSSAnimate from './UI/CSSAnimate';
-import Alerts from './UI/Alerts';
-import Button from './UI/Button';
+
+import Login from './Sign/Login'; // 登录
+import Register from './Sign/Register'; // 注册
+import RetrievePassword from './Sign/RetrievePassword'; // 找回密码
+import Dept from './Crud/Dept'; // 部门管理
+import User from './Crud/User'; // 用户管理
+import Role from './Crud/Role'; // 角色管理
+import Resource from './Crud/Resource'; // 权限（资源）管理
+import Attachment from './Crud/Attachment'; // 附件管理
+import Mail from './Crud/Mail'; // 邮件管理
 
 /**
  * 主路由配置
@@ -86,32 +50,6 @@ const routesConfig = app => [
             Resource(app),
             Attachment(app),
             Mail(app),
-
-            Blank(app),
-            Toolbar(app),
-            Column(),
-            SearchBar(),
-            EC(app),
-            G2(app),
-            Icon(),
-            Mask(),
-            Editor(),
-            CSSAnimate(),
-            Alerts(),
-            Button(),
-            DataTable(app),
-            Form(app),
-            TransferTree(app),
-            BaseComponent(),
-            Coming(),
-            ScreenLock(),
-            Gallery(),
-            Result(),
-            Page403(),
-            Page500(),
-            Print(),
-            Banner(app),
-            LevelRoute(app),
             NotFound()
         ]
     }
