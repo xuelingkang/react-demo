7cf811116c1bf601f1ed495cee08ff793e9b1e29
xuelingkang <574290057@qq.com>
2019-06-11 12:46:36 +0800

    增加windows下自动编译+打包脚本

---
+++ build.bat
@@ -0,0 +1,34 @@
+:: 将编译好的文件打包到桌面
+@echo off
+:: 编译输出目录
+set buildDir=%cd%\build
+:: 最终压缩包路径
+set zipFile=%USERPROFILE%\Desktop\build.zip
+:: zip程序路径
+set zipExe=%cd%\win\zip.exe
+
+:: 删除编译输出目录
+if exist %buildDir% (
+echo clean %buildDir%
+echo.
+call rd/s/q %buildDir%
+)
+:: 删除原有压缩包
+if exist %zipFile% (
+echo clean %zipFile%
+echo.
+call del %zipFile%
+)
+:: 变异
+echo build...
+echo.
+call npm run-script build
+:: 压缩
+echo compress...
+echo.
+cd %buildDir%
+call %zipExe% -r %zipFile% .\
+:: 完成
+echo.
+echo complete...
+pause>null
