5b154827d07c3b6ebd0c8e6cd5c99ca1a2db7d9b
薛凌康 <574290057@qq.com>
2019-08-20 19:00:43 +0800

    修改提交请求

--- src/routes/Crud/Letter/service/index.js
+++ src/routes/Crud/Letter/service/index.js
@@ -1,12 +1,12 @@
-import { axiosGet, axiosDelete, axiosPost } from '@/utils/axios';
+import { axiosGet, axiosDelete, axiosPost, JSON_HEADER } from '@/utils/axios';
 
-export const save = payload => axiosPost('/letter', payload);
+export const save = payload => axiosPost('/letter', payload, JSON_HEADER);
 
 export const detail = payload => axiosGet('/letter/{id}', payload);
 
 export const del = payload => axiosDelete('/letter/{ids}', payload);
 
-export const saveReply = payload => axiosPost('/letterReply', payload);
+export const saveReply = payload => axiosPost('/letterReply', payload, JSON_HEADER);
 
 export const delReply = payload => axiosDelete('/letterReply/{ids}', payload);
 
